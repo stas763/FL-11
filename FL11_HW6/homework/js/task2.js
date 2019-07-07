0ad43a6b682c2eb3ba37a719998713c6f707c451
Stan Kleshnov <s.v.kleshnov@gmai.com>
2019-07-07 23:48:32 +0300

    ‘Isosceles triangle’

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -4,4 +4,9 @@ const c = prompt('Enter values triangle side length C');
 
 if (a + b > c && a + c > b && b + c > a) {
     console.log('Normal triangle');
+    if ( a === b === c) {
+    console.log('Isosceles triangle');
+    }
+  } else {
+    console.log('Triangle doesn’t exist');
   }
