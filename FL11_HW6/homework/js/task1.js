ddb4fc33335ddf26c7a73e47917fff4f963b07f9
Stan Kleshnov <s.v.kleshnov@gmai.com>
2019-07-07 20:43:13 +0300

    First try

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,12 @@
-// Your code goes here
+const a1 = prompt('Enter values x for point A');
+const a2 = prompt('Enter values y for point A');
+const b1 = prompt('Enter values x for point B');
+const b2 = prompt('Enter values y for point B');
+const c1 = prompt('Enter values x for point C');
+const c2 = prompt('Enter values y for point C');
+
+const HALF = 2;
+
+const isMidpoint = parseInt(a1, 10) + parseInt(b1, 10) / HALF === parseInt(c1, 10) && (parseInt(a2, 10) + parseInt(b2, 10)) / HALF === parseInt(c2, 10);
+
+alert('Middle point result: ' + isMidpoint);
