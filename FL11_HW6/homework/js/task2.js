76095534a9e77f9e1b841086ff4cd6c4afade618
Stan Kleshnov <s.v.kleshnov@gmai.com>
2019-07-07 23:37:15 +0300

    Edited task2.js

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,7 @@
-// Your code goes here
+const a = prompt('Enter values triangle side length A');
+const b = prompt('Enter values triangle side length B');
+const c = prompt('Enter values triangle side length C');
+
+if (a + b > c && a + c > b && b + c > a) {
+    console.log('Normal triangle');
+  }
