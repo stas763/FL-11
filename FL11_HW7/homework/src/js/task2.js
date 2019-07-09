bf28e727a23d9ea07f4c11df40e1333567b42ac2
Stan Kleshnov <s.v.kleshnov@gmai.com>
2019-07-09 22:34:07 +0300

    Edited

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,4 +1,3 @@
-let start = confirm('Do you want to play a game?');
-if (start === '' || start === null) {
-  alert('You did not become a billionaire, but can.')
-}
+let isStart = confirm('Do you want to play a game?');
+
+  alert('You did not become a billionaire, but can.');
