f6362bb12d0b41b9f1d6f501c24ab227d16d241b
Stan Kleshnov <s.v.kleshnov@gmai.com>
2019-07-09 14:35:45 +0300

    Step 1. Check login

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -1,1 +1,9 @@
-// Your code goes here
+const userMail = prompt('Enter your email');
+const minStMail = 6;
+
+
+if (userMail === '' || userMail === null ) {
+  alert('Canceled');
+} else if (userMail && userMail.length <= minStMail) {
+  alert('I dont know any emails having name length less than 6 symbols');
+}
