ac674b65f08291de57e98e4028c8a04fa0017669
Stan Kleshnov <s.v.kleshnov@gmai.com>
2019-07-16 20:20:31 +0300

    Function - 0

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,1 +1,17 @@
-// Your code goes here
+//0. Write function, which returns array of numbers from string parameter.
+let finish = [];
+
+function getNumbers(string) {
+  for (let n = 0; n<string.length; n++){
+    if (!isNaN(+string[n])){
+      finish [finish.length]=+parseInt(string[n]);n++;
+    }
+  }
+  return finish;
+}
+
+
+// returns []
+console.log(getNumbers('string'));
+ // returns [1,3,9,5]
+console.log(getNumbers('n1um3ber95'));
