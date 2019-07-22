12d2d1eb8899a4c8466787c35cc669822174ee04
Stan Kleshnov <s.v.kleshnov@gmai.com>
2019-07-22 14:52:29 +0300

    Created player

--- FL11_HW10/homework/js/fighters_game.js
+++ FL11_HW10/homework/js/fighters_game.js
@@ -1,1 +1,28 @@
-/* Your code goes here */
+
+class Fighter {
+  constructor(player) {
+    this._name = player.name;
+    this._damage = player.damage;
+    this._agility = player.agility;
+    this._healts = player.health;
+  }
+
+  getName() {
+    return this._name;
+    // console.log(name);
+  }
+  getDamage() {
+    return this._damage;
+    // console.log(damage);
+  }
+  getAgility() {
+    return this._agility;
+    // console.log(agility);
+  }
+  getHealth() {
+    return this._health;
+    // console.log(health);
+  }
+
+
+}
